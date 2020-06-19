547d6437ac102d1e8119ba4af874cbe1c0fcc5f0
Firas Wehbe <firaswehbe@users.noreply.github.com>
2020-06-18 22:49:19 -0500

    fixed package name

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="firaswehbe", 
+    name="redcap2sqlalchemy", 
     version="0.0.1",
     author="Firas H. Wehbe",
     author_email="Firas Wehbe <firas.wehbe@northwestern.edu>",
