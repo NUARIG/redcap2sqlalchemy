8bdb441054c575c2b97492564937f05ab7cbe1ec
Firas Wehbe <firaswehbe@users.noreply.github.com>
2020-06-18 19:32:55 -0500

    Initial set up for package

---
+++ setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="firaswehbe", 
+    version="0.0.1",
+    author="Firas H. Wehbe",
+    author_email="Firas Wehbe <firas.wehbe@northwestern.edu>",
+    description="Extracting data from REDCapo and pushing into a relational DB",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/NUARIG/redcap2sqlalchemy",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Healthcare Industry",
+        "Intended Audience :: Science/Research",
+        "Topic :: Database",
+    ],
+    python_requires='>=3.6',
+)
