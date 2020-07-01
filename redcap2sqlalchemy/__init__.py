0e4126022c2e453993318f5d635cd75bcb4ec34e
Firas Wehbe <firaswehbe@users.noreply.github.com>
2020-06-30 20:44:51 -0500

    WIP

--- redcap2sqlalchemy/__init__.py
+++ redcap2sqlalchemy/__init__.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
 """Light weight and flexible module to export data from REDCap project to a relational database using SQLAlchemy"""
 
-from .rcproject import RCProject, REDCapError, rcproject_from_config
+from .rcproject import *
 from .ddlutils import *
--- redcap2sqlalchemy/ddlutils.py
+++ redcap2sqlalchemy/ddlutils.py
@@ -1,4 +1,21 @@
 import sqlalchemy as _sa
+from .rcproject import RCProject, REDCapError
+
+"""
+The following classes and functions are structural components (connection, Table, ...)
+"""
+
+def tablegen_rcform(myrcproject, myformname, mytablename, mymetadata):
+    """Generates a SQL Alchemy Table from a project, a form name, and Metada object"""
+    if myformname not in map( lambda x:x['instrument_name'], myrcproject.exportInstruments() ):
+        raise REDCapError('Form name is not in the given project')
+
+    return _sa.Table(mytablename, mymetadata)
+
+
+"""
+The following classes and function address the DDL and ETL logic
+"""
 
 class RC2SARule:
     """A rule used to match and act based on patterns"""
@@ -11,22 +28,21 @@ class RC2SARule:
         self.transformfn = transformfn
 
     def __repr__(self):
-        return('<redcap2sqlalchemy::RC2SARule {0}>'.format(self.name))
-
-
+        return('<redcap2sqlalchemy::RC2SARule {0} >'.format(self.name))
 
 class RC2SARuleQueue:
     """Ordered set of Rules"""
-    pass
+
+    def __init__(self):
+        raise NotImplementedError('RC2SARuleQueue is not implemented yet')
 
 def match_always (fieldinfo):
+    """This matching function always returns True, good to keep at the end of a queue as catchall"""
     return True
 
 def colgen_string (fieldinfo):
+    _sa.DDL
     raise NotImplementedError('Column generation function colgen_string not implemented yet')
 
 def transform_asis_str (value):
     return(str(value))
-
-class RC2SATable(_sa.Table):
-    pass
